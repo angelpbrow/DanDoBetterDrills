1d6a53052a83081cc9b7ea839545e66066a76591
Angelika Brown <angelikpbrown@gmail.com>
2025-03-04 16:11:01 -0500

    It begins

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,8 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+       return x > y;
+        //return null;
     }
 
     /**
